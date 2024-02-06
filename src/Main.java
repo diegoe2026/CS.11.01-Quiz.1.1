b16f9607d0c1e007b7c2c1f8ca95d6b290bc6cc2
diegoe2026 <157024760+diegoe2026@users.noreply.github.com>
2024-02-06 11:22:45 +0800

    my quiz

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-
-
-
+        Scanner SCAN = new Scanner (System.in);
+        int age;
+        String fn;
+        String favouriteFood;
+        System.out.println("age please:");
+        age = SCAN.nextInt();
+        System.out.println("whats ur first name:");
+        fn = SCAN.next();
+        System.out.println("whats ur fav food:");
+        favouriteFood = SCAN.next();
+        System.out.println("first name:" + fn);
+        System.out.println("age: " + age);
+        System.out.println("Favourite food: " + favouriteFood);
     }
 
 }
